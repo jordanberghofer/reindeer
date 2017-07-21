bd387eee3f539fe6ecd999cf6fb22820f7a3bc10
jordanberghofer <jordan.berghofer@gmail.com>
2017-07-21 15:11:07 -0500

    final reindeer

--- reindeer.js
+++ reindeer.js
@@ -1,15 +1,24 @@
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
-var combo = "";
+// var combo = "";
 
 var hohohoElement = document.getElementById("reindeer");
-debugger;
+// debugger;
 
-reindeerGames(combo);
+// reindeerGames();
 
-function reindeerGames(combo) {
-    // debugger;
-    for (var i = 0; i < reindeer.length ; i++) {
-       hohohoElement.innerHTML += `<li>$(colors[i] + " " + reindeer[i] " "</li>`;
-       }
+// function reindeerGames(combo) {
+//     // debugger;
+//     for (var i = 0; i < reindeer.length ; i++) {
+//        hohohoElement.innerHTML += `<li>$(colors[i] + " " + reindeer[i] " "</li>`;
+//        }
+// }
+
+var combo = "";
+
+for (var i = 0; i < reindeer.length; i++) {
+    combo += " " + colors[i] + "-" + reindeer[i];
 }
+
+
+console.log("combo", combo);
